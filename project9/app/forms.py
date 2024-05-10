cb67acead89a1b713076e114ba09140e918695a3
Mamarani Sahoo <168465801+mamarani2001@users.noreply.github.com>
2024-05-10 09:17:51 +0530

    Add files via upload

---
+++ project9/app/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email','username','password']
+        help_texts={'username':''}
+        widgets={'password':forms.PasswordInput}
+        
+        
